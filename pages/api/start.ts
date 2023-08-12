fb9dda87ecab4be1cc21b875d0027b67e3252a53
DIEHARDERS <cybrobo@gmail.com>
2023-08-11 17:32:02 -0700

    Added re-writes to internal processes.

--- pages/api/start.ts
+++ pages/api/start.ts
@@ -1,4 +1,3 @@
-import Cors from "cors";
 import { exec } from "child_process";
 import enableCors from "../../src/utils/enableCors";
 import type { NextApiRequest, NextApiResponse } from "next";
@@ -10,15 +9,20 @@ const handler = (req: NextApiRequest, res: NextApiResponse) => {
       return;
     }
     const host = "127.0.0.1";
-    const port = req.body?.port || "8000";
+    const port = req.body?.port || "8000"; // Default port should match the target library's port
     console.log("@@ Starting inference server on", port);
 
     // Start the inference server
     // exec(`npm run fastapi-dev:start --host=127.0.0.1 --port=${port}`);
-    // @TODO Determine if this is already running. Could set a state var in GUI that prevents another startup pressed.
+
+    // @TODO This was prev used to run the universla api but this should now run the target inference app
     const { stdout, stderr, pid } = exec(
       `python -m uvicorn api.index:app --host ${host} --port ${port} --reload`
     );
+    stdout?.on("data", (data) => {
+      console.log("@@ Inference server log:", data);
+    });
+
     // check running process on port `lsof -i :8000`
     // nestat -aon | findstr 123456
     if (stderr?.errored) {
