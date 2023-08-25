af79163eea4b38bb46bad43fad20638dd3a160c2
DIEHARDERS <cybrobo@gmail.com>
2023-08-25 11:50:55 -0700

    Move app links to own component

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@
 import { useEffect, useState } from 'react'
 import ModelCard from '../src/frontend/components/text-model-browser/ModelCard'
 import textModels, { IModelCard } from '../models/models'
+import AppsBrowser from '../src/frontend/components/AppsBrowser'
 
 export default function Home() {
   // Local Storage keys
@@ -10,7 +11,6 @@ export default function Home() {
   const ITEM_CURRENT_MODEL = 'current-text-model' // string
   const ITEM_TEXT_MODELS = 'text-models-list' // array<string>
   // App vars
-  const appLink = 'https://brain-dump-dieharders.vercel.app/'
   const ip = 'http://localhost:8008'
   const [isStarted, setIsStarted] = useState(false)
   const [modelPath, setModelPath] = useState<string>(localStorage.getItem(ITEM_MODEL_PATH) || '')
@@ -249,80 +249,6 @@ export default function Home() {
       </div>
     )
   }
-  // Browse Apps menu
-  const renderAppsMenu = () => {
-    return (
-      <div className="z-5 mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href={appLink}
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-xl font-semibold">
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information and share it. Search and analyze private data with agents.
-          </p>
-        </a>
-
-        <a
-          href={appLink}
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-xl font-semibold">
-            Create{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find inspiration, kick-off a project or just toss ideas around with a creative avatar.
-          </p>
-        </a>
-
-        <a
-          href={appLink}
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-xl font-semibold">
-            Heal{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore your mind then reflect on your journey with an ai buddy by your side.
-          </p>
-        </a>
-
-        <a
-          href={appLink}
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-xl font-semibold">
-            Grow{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Plan, adapt, enact. Take advantage of critical thinking processes to reach your goals.
-          </p>
-        </a>
-      </div>
-    )
-  }
 
   // Intialize default model path if non selected/stored
   useEffect(() => {
@@ -363,7 +289,7 @@ export default function Home() {
       </div>
 
       {/* Footer menus */}
-      {isStarted ? renderAppsMenu() : <TextModelBrowserMenu data={textModels} />}
+      {isStarted ? <AppsBrowser /> : <TextModelBrowserMenu data={textModels} />}
     </main>
   )
 }
