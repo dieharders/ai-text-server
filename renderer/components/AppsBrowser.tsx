32f5743aaf81c35816b8feda033ad0a5a00a6cad
DIEHARDERS <cybrobo@gmail.com>
2023-10-19 19:42:13 -0700

    Add symbols to app links

--- renderer/components/AppsBrowser.tsx
+++ renderer/components/AppsBrowser.tsx
@@ -17,7 +17,7 @@ const AppsBrowser = () => {
         <h2 className="mb-3 text-xl font-semibold">
           Learn{' '}
           <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-            -&gt;
+            📚
           </span>
         </h2>
         <p className="m-0 max-w-[30ch] text-sm opacity-50">
@@ -34,7 +34,7 @@ const AppsBrowser = () => {
         <h2 className="mb-3 text-xl font-semibold">
           Create{' '}
           <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-            -&gt;
+            ✨
           </span>
         </h2>
         <p className="m-0 max-w-[30ch] text-sm opacity-50">
@@ -51,7 +51,7 @@ const AppsBrowser = () => {
         <h2 className="mb-3 text-xl font-semibold">
           Heal{' '}
           <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-            -&gt;
+            💖
           </span>
         </h2>
         <p className="m-0 max-w-[30ch] text-sm opacity-50">
@@ -68,7 +68,7 @@ const AppsBrowser = () => {
         <h2 className="mb-3 text-xl font-semibold">
           Grow{' '}
           <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-            -&gt;
+            📈
           </span>
         </h2>
         <p className="m-0 max-w-[30ch] text-sm opacity-50">
