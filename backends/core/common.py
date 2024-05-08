fa43cedfacbb70fbdd6db1649f48de0832276483
DIEHARDERS <cybrobo@gmail.com>
2024-05-07 23:36:01 -0700

    HBAI-229 Use chroma HttpClient

--- backends/core/common.py
+++ backends/core/common.py
@@ -6,7 +6,7 @@ import glob
 import httpx
 import subprocess
 from typing import Any, List, Optional, Tuple
-from classes import (
+from core.classes import (
     CHAT_MODES,
     InstalledTextModelMetadata,
     InstalledTextModel,
