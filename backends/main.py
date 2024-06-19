85d969924e180ba4936755682fdb7c25228e37f2
DIEHARDERS <cybrobo@gmail.com>
2024-06-18 23:15:01 -0700

    HBAI-279 Upping version

--- backends/main.py
+++ backends/main.py
@@ -28,7 +28,7 @@ from storage.route import router as storage
 
 
 server_info = None
-api_version = "0.6.2"
+api_version = "0.6.3"
 SERVER_PORT = 8008
 # Display where the admin can use the web UI
 openbrew_studio_url = "https://studio.openbrewai.com"
