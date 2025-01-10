5592bb22d7d0978a9b3cf7d6043e6729e8a38f07
DIEHARDERS <cybrobo@gmail.com>
2025-01-09 17:19:32 -0800

    break out gui logic from api server

--- backends/main.py
+++ backends/main.py
@@ -1,37 +1,20 @@
 import os
-import signal
 import sys
+import time
 import threading
-import uvicorn
 import webbrowser
-import httpx
-import socket
-import pyqrcode
 import tkinter as tk
 from tkinter import ttk
-from dotenv import load_dotenv
-from fastapi import (
-    FastAPI,
-    Request,
-    APIRouter,
-)
-from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import HTMLResponse
-from fastapi.templating import Jinja2Templates
-from contextlib import asynccontextmanager
-from embeddings import storage as vector_storage
-from core import common, classes
-from services.route import router as services
-from embeddings.route import router as embeddings
-from inference.route import router as text_inference
-from storage.route import router as storage
+from core import common
 
 
-server_info = None
-api_version = "0.7.2"
-SERVER_PORT = 8008
-# Display where the admin can use the web UI
-openbrew_studio_url = "https://studio.openbrewai.com"
+class AppState:
+    class State:
+        def __init__(self):
+            return
+
+    def __init__(self):
+        self.state = self.State()
 
 
 # Parse runtime arguments passed to script
@@ -59,67 +42,23 @@ is_debug = hasattr(sys, "gettrace") and sys.gettrace() is not None
 is_dev = build_env["mode"] == "dev" or is_debug
 is_prod = build_env["mode"] == "prod" or not is_dev
 is_headless = build_env["headless"] == "True"  # headless == no UI window shown
+
 # Comment out if you want to debug on prod build
 if is_prod:
     # Remove prints in prod when deploying in window mode
     sys.stdout = open(os.devnull, "w")
     sys.stderr = open(os.devnull, "w")
 
-# Path to the .env file in either the parent or /_deps directory
-try:
-    # Look in app's _deps dir
-    if sys._MEIPASS:
-        env_path = common.dep_path(".env")
-except Exception:
-    # Otherwise look in codebase root dir
-    current_directory = os.path.dirname(os.path.abspath(__file__))
-    parent_directory = os.path.dirname(current_directory)
-    env_path = os.path.join(parent_directory, ".env")
-load_dotenv(env_path)
-
-
-@asynccontextmanager
-async def lifespan(application: FastAPI):
-    print(f"{common.PRNT_API} Lifespan startup", flush=True)
-    # https://www.python-httpx.org/quickstart/
-    app.requests_client = httpx.Client()
-    # Initialize global data here
-    application.state.PORT_HOMEBREW_API = SERVER_PORT
-    application.state.db_client = None
-    application.state.llm = None  # Set each time user loads a model
-    application.state.path_to_model = ""  # Set each time user loads a model
-    application.state.model_id = ""
-    application.state.embed_model = None
-    app.state.loaded_text_model_data = {}
-    app.state.is_prod = is_prod
-    app.state.is_dev = is_dev
-    app.state.is_debug = is_debug
-
-    yield
-    # Do shutdown cleanup here...
-    print(f"{common.PRNT_API} Lifespan shutdown")
-
-
-app = FastAPI(title="Obrew🍺Server", version=api_version, lifespan=lifespan)
-
-# Get paths for SSL certificate
-SSL_ENABLED = os.getenv("ENABLE_SSL", "False").lower() in ("true", "1", "t")
-SSL_KEY: str = common.dep_path(os.path.join("public", "key.pem"))
-SSL_CERT: str = common.dep_path(os.path.join("public", "cert.pem"))
-XHR_PROTOCOL = "http"
-if SSL_ENABLED is True:
-    XHR_PROTOCOL = "https"
-# Configure CORS settings
-CUSTOM_ORIGINS_ENV: str = os.getenv("CUSTOM_ORIGINS")
-CUSTOM_ORIGINS = CUSTOM_ORIGINS_ENV.split(",") if CUSTOM_ORIGINS_ENV else []
-origins = [
-    "http://localhost:3000",  # (optional) for testing client apps
-    # "https://hoppscotch.io",  # (optional) for testing endpoints
-    # "https://brain-dump-dieharders.vercel.app",  # (optional) client app origin (preview)
-    # "https://homebrew-ai-discover.vercel.app",  # (optional) client app origin (production/alias)
-    openbrew_studio_url,  # (required) client app origin (production/domain)
-    *CUSTOM_ORIGINS,
-]
+# Initialize global data here
+server_info = None
+SERVER_PORT = 8008
+application = AppState()
+application.state.PORT_HOMEBREW_API = SERVER_PORT
+application.state.is_prod = is_prod
+application.state.is_dev = is_dev
+application.state.is_debug = is_debug
+application.keep_open = True
+openbrew_studio_url = "https://studio.openbrewai.com"  # the web UI
 
 
 ###############
@@ -127,49 +66,19 @@ origins = [
 ###############
 
 
-def shutdown_server(*args):
-    print(f"{common.PRNT_API} Server forced to shutdown.", flush=True)
-    os.kill(os.getpid(), signal.SIGTERM)  # or SIGINT
-    # sys.exit(0)
-
-
-def display_server_info():
-    # Display the local IP address of this server
-    hostname = socket.gethostname()
-    IPAddr = socket.gethostbyname(hostname)
-    remote_ip = f"{XHR_PROTOCOL}://{IPAddr}"
-    local_ip = f"{XHR_PROTOCOL}://localhost"
-    return {
-        "local_ip": local_ip,
-        "remote_ip": remote_ip,
-    }
-
-
 def start_server():
     try:
-        # Start the ASGI server (https)
-        if XHR_PROTOCOL == "https":
-            print(f"{common.PRNT_API} Starting API server with SSL.")
-            uvicorn.run(
-                app,
-                host="0.0.0.0",
-                port=SERVER_PORT,
-                log_level="info",
-                # Include these to host over https. If server fails to start make sure the .pem files are generated in _deps/public dir
-                ssl_keyfile=SSL_KEY,
-                ssl_certfile=SSL_CERT,
-            )
-        # Start the ASGI server (http)
-        else:
-            print(f"{common.PRNT_API} Starting API server.")
-            uvicorn.run(
-                app,
-                host="0.0.0.0",
-                port=SERVER_PORT,
-                log_level="info",
-            )
+        # Start the API server
+        print(f"{common.PRNT_API} Starting API server.")
     except Exception as e:
         print(f"{common.PRNT_API} API server shutdown. {e}")
+
+
+def shutdown_server(*args):
+    # send shutdown server request
+    # ...
+    print(f"{common.PRNT_API} Server forced to shutdown.", flush=True)
+    application.keep_open = False
 
 
 def run_app_window():
@@ -181,10 +90,8 @@ def run_app_window():
 
 
 def open_browser():
-    # Find IP info
-    server_info = display_server_info()
-    local_ip = server_info["local_ip"]
-    local_url = f"{local_ip}:{SERVER_PORT}"
+    ip = "localhost"
+    local_url = f"{ip}:{SERVER_PORT}"
     # Open browser to WebUI
     print(f"{common.PRNT_API} Opening WebUI at {local_url}")
     webbrowser.open(local_url, new=2)
@@ -226,115 +133,20 @@ def GUI():
     shutdown_server()
 
 
-##############
-### Routes ###
-##############
-
-# Redirect requests to our custom endpoints
-# from fastapi import Request
-# @app.middleware("http")
-# async def redirect_middleware(request: Request, call_next):
-#     return await redirects.text(request, call_next, str(app.PORT_TEXT_INFERENCE))
-
-# Add CORS support
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-# Import routes
-endpoint_router = APIRouter()
-endpoint_router.include_router(services, prefix="/v1/services", tags=["services"])
-endpoint_router.include_router(embeddings, prefix="/v1/memory", tags=["embeddings"])
-endpoint_router.include_router(storage, prefix="/v1/persist", tags=["storage"])
-endpoint_router.include_router(
-    text_inference, prefix="/v1/text", tags=["text inference"]
-)
-app.include_router(endpoint_router)
-
-
-# Return a "connect" GUI page for user to config and startup the API server,
-# then return the user to the supplied callback url with query params of config added.
-# QRcode generation -> https://github.com/arjones/qr-generator/tree/main
-@app.get("/", response_class=HTMLResponse)
-async def connect_page(request: Request):
-    # Be sure to link `public/templates` to the app's dependency dir (_deps) via PyInstaller
-    templates_dir = common.dep_path(os.path.join("public", "templates"))
-    templates = Jinja2Templates(directory=templates_dir)
-    remote_url = server_info["remote_ip"]
-    local_url = server_info["local_ip"]
-    # Generate QR code - direct to remote url
-    qr_code = pyqrcode.create(
-        f"{remote_url}:{SERVER_PORT}/?hostname={remote_url}&port={SERVER_PORT}"
-    )
-    qr_data = qr_code.png_as_base64_str(scale=5)
-    # qr_image = qr_code.png("image.png", scale=8) # Write image file to disk
-
-    return templates.TemplateResponse(
-        "index.html",
-        {
-            "request": request,
-            "qr_data": qr_data,
-            "title": "Connect to Obrew Server",
-            "app_name": "Obrew🍺Server",
-            "message": "Scan the code with your mobile device to access the WebUI remotely and/or click the link below.",
-            "host": local_url,
-            "remote_host": remote_url,
-            "port": SERVER_PORT,
-        },
-    )
-
-
-# Keep server/database alive
-@app.get("/v1/ping")
-def ping() -> classes.PingResponse:
-    try:
-        db = vector_storage.get_vector_db_client(app)
-        db.heartbeat()
-        return {"success": True, "message": "pong"}
-    except Exception as e:
-        print(f"{common.PRNT_API} Error pinging server: {e}")
-        return {"success": False, "message": ""}
-
-
-# Tell client we are ready to accept requests
-@app.get("/v1/connect")
-def connect() -> classes.ConnectResponse:
-    return {
-        "success": True,
-        "message": f"Connected to api server on port {SERVER_PORT}. Refer to '{XHR_PROTOCOL}://localhost:{SERVER_PORT}/docs' for api docs.",
-        "data": {
-            "docs": f"{XHR_PROTOCOL}://localhost:{SERVER_PORT}/docs",
-            "version": api_version,
-        },
-    }
-
-
 #############
 ### Start ###
 #############
 
 
 if __name__ == "__main__":
+    print(f"Starting app...", flush=True)
     try:
         # Show a window
         if not is_headless:
             run_app_window()
-        # Start API server
-        print(
-            f"{common.PRNT_API} Navigate your browser to:\n-> {openbrew_studio_url} for WebUI",
-            flush=True,
-        )
-        server_info = display_server_info()
-        local_ip = server_info["local_ip"]
-        remote_ip = server_info["remote_ip"]
-        print(
-            f"{common.PRNT_API} Refer to API docs:\n-> {local_ip}:{SERVER_PORT}/docs \nOR\n-> {remote_ip}:{SERVER_PORT}/docs",
-            flush=True,
-        )
-        start_server()
-    except (KeyboardInterrupt, Exception):
+        # Block main thread to keep program open
+        while application.keep_open:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        # Close everything and cleanup
         shutdown_server()
